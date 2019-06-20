5955ae62c445798772a73ba098857fe31d17861b
s20181106118 <s20181106118@Mac28.local>
2019-06-20 12:01:23 +0800

    chengjie
    caipan

--- chengji/main.cpp
+++ chengji/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
-
+# include<fstream>
+# include<string>
+using namespace std;
+struct student{
+    char num[11];
+    char name[11];
+    char xueyuan[11];
+};
+struct referee{
+    char name[7];
+    float  score[7];
+}s1;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream in( "/users/s20181106118/Desktop/a.txt/",ios_base::out);
+    ifstream cin("/users/s20181106118/Desktop/b.txt/",ios_base::out);
+    ofstream out("/users/s20181106118/Desktop/c.txt/",ios_base::out);
+    int i,k,j;
+    struct student s[11];
+    for(i=0;i<3;i++){
+    gets(s[i].name);
+    gets(s[i].num);
+    gets(s[i].xueyuan);
+        }
+    
+    for(k=0;k<7;k++){
+        gets(s1.name);
+       
+    }
+   
+    
     return 0;
 }
+
